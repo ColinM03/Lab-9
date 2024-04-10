a8a2280bee28ebae559c47fa4eb77873d0d947a4
colin <cmceliece@ufl.edu>
2024-04-10 16:47:16 -0400

    1st commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Colin McEliece
+from decoder import *
+def main():
+    while True:
+        print("Menu")
+        print("------------- ")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit \n")
+        choice = int(input("Please enter an option: "))
+        if choice == 1:
+            pass
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        else:
+            print("Invalid input. \n")
